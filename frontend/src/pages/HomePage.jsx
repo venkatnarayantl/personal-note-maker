4ab61bec9331ec7ef242848b084e5c3abc79bf6a
Venkat Narayan TL <venkatnarayantl@gmail.com>
2026-03-07 10:46:06 +0530

    Delete functionality done

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -4,6 +4,8 @@ import RateLimitedUI from '../components/RateLimitedUI'
 import toast from 'react-hot-toast';
 import NoteCard from '../components/NoteCard';
 import api from '../libs/axios';
+import NotesNotFound from '../components/NotesNotFound';
+
 
 
 function HomePage() {
@@ -45,11 +47,12 @@ function HomePage() {
 
       <div className='max-w-7xl mx-auto p-4 mt-6'>
         {loading && <div className='text-center text-primary py-6'>Loading...</div>}
+        {notes.length ===0 && !isRateLimited && <NotesNotFound/>}
 
         {notes.length > 0 && !isRateLimited && (
           <div className='grid grid-cols-1 md: grid-cols-2 lg:grid-cols-3 gap-10'>
             {notes.map((note)=>(
-              <NoteCard key={note._id} note={note} />
+              <NoteCard key={note._id} note={note} setNotes={setNotes} />
             ))}
           </div>
         )}
